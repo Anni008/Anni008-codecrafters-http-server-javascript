4e2acdaf7345853ad27a27e8acb220eba46c9778
Anni008 <ankitsingh110419@gmail.com>
2025-01-07 21:09:07 +0530

    attempt 2 - Extract URL path

--- app/main.js
+++ app/main.js
@@ -6,9 +6,7 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    // console.log("received data: ", data.toString());
     const requestData = data.toString();
-    console.log(requestData);
     const requestLine = requestData.split("\r\n")[0];
     const urlPath = requestLine.split(" ")[1];
     if (urlPath.length > 1) {
@@ -16,7 +14,6 @@ const server = net.createServer((socket) => {
     } else {
       socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
     }
-    console.log("extra", urlPath);
     socket.end();
   });
   socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
